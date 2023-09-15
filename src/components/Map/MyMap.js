201816388ce57d6155ceb9e75259bc6ba7b61950
taggart-comet <tmv790@gmail.com>
2023-09-15 20:10:57 +0300

    list

--- src/components/Map/MyMap.js
+++ src/components/Map/MyMap.js
@@ -2,7 +2,7 @@ import {Map, YMaps} from "react-yandex-maps";
 import React from "react";
 
 
-function MyMap() {
+function MyMap({ livingCompounds }) {
     return (
             <YMaps width="100%" height="100%" query={{apiKey: process.env.YANDEX_API_KEY}}>
                 <Map defaultState={{center: [55.75, 37.57], zoom: 13}} width="100%" height="100%"/>
